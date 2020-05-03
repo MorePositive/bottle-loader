380b12bee4c2470179bb2789f4e942c6b1eeea18
Ilya <work@ilya.pm>
2020-05-03 00:14:19 -0400

    I cleaned some dirty stuff in there

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
+const parallaxEl = document.querySelector('.parallax');
+const loadingEl = document.querySelector('#loading');
 const progressEl = document.querySelector('.loader .progress');
 const bubblesEl = document.querySelector('.bubbles');
-
-const progress = function(value) {
-	value = Math.max(0, Math.min(90, value));
-	progressEl.style.top = (100-value) + '%';
-}
 
 const bubble = function() {
 	var bubble = document.createElement('div');
@@ -18,19 +15,30 @@ const bubble = function() {
 	}, 2000);
 }
 
-const progressiveLoad = function(state) {
-	if(state >= 100) return loaded();
+const progressiveLoad = function(state, done) {
+	if(state >= 100) return done && done();
 	state += Math.round(Math.random()*25);
-	progress(state);
+	var value = Math.max(0, Math.min(90, state));
+	progressEl.style.top = (100-value) + '%';
+
 	(state % 2) && bubble();
-	setTimeout(progressiveLoad.bind(this, state), Math.random()*400);
+	setTimeout(progressiveLoad.bind(this, state, done), Math.random()*400);
 }
 
-const loaded = function() {
-	document.querySelector('.parallax').scroll({
-		top: window.innerHeight,
-		left: 0,
-		behavior: 'smooth'
-	});
+const scrollSlide = function(from, to, duration, start, done) {
+	start = start || performance.now();
+	const now = performance.now();
+	const time = Math.min(1, ((now - start) / duration));
+	const timeFn = function(t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 };
+	parallaxEl.scrollTop = Math.ceil((timeFn(time) * (to - from)) + from);
+	if(parallaxEl.scrollTop >= to) return done && done();
+
+	requestAnimationFrame(scrollSlide.bind(this, from, to, duration, start, done));
 }
-progressiveLoad(0);
+
+progressiveLoad(0, function() {
+	document.body.classList.remove('loading');
+	scrollSlide(0, window.innerHeight, 1000, null, function() {
+		loadingEl.classList.add('hidden');
+	});
+});
